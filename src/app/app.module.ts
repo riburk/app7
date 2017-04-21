8e461f4cae335dff50b6ead016b0d92e454a2461
Richard Burkhardt <Richard.Burkhardt@apexlearning.com>
2017-04-21 11:17:54 -0700

    adding a component and a directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { HighlightDirective } from './my-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
